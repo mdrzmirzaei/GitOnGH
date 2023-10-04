67ffb75c6c87a379c1c6c40adfbda7901e872db1
mdrzmirzaei <mdrz.mirzaei@gmail.com>
2023-10-05 00:32:53 +0330

    this is next 2th change

--- src/main/java/org/git/Main.java
+++ src/main/java/org/git/Main.java
@@ -2,6 +2,8 @@ package org.git;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("this is last change");
     }
 }
