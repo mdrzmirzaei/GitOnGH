a328f2edffa089b79fd42e344707f80c48e6b7d9
MohammadReza Mirzaei <83281027+mdrzmirzaei@users.noreply.github.com>
2023-10-05 00:34:52 +0330

    Update Main.java this is mohammadrezaaaaa

--- src/main/java/org/git/Main.java
+++ src/main/java/org/git/Main.java
@@ -2,7 +2,7 @@ package org.git;
 
 public class Main {
     public static void main(String[] args) {
-
+System.out.println("this is mohammad reza!!!!!");
         System.out.println("Hello world!");
         System.out.println("this is last change");
     }
