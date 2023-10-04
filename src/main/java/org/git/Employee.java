8982b330562dcd5f83846319936b9b21493e52ee
mdrzmirzaei <mdrz.mirzaei@gmail.com>
2023-10-05 00:38:20 +0330

    class Employee is added

---
+++ src/main/java/org/git/Employee.java
@@ -0,0 +1,37 @@
+package org.git;
+
+public class Employee {
+    int age;
+    String name;
+    String family;
+
+    public Employee(int age, String name, String family) {
+        this.age = age;
+        this.name = name;
+        this.family = family;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+}
